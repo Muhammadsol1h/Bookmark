7038d83bcd610c850ab6528c3d80d7952309e8c6
Muhammadsol1h <mmuhammadsolih0@gmail.com>
2024-12-22 08:50:43 -0800

    add

--- js/app.js
+++ js/app.js
@@ -22,3 +22,66 @@ featuresBtn.forEach((item) => {
     });
 });
 
+let oneBtn = document.querySelector('#oneBtn');
+let oneCart = document.querySelector('#oneCart');
+let hidden = document.querySelector('#hidden');
+
+if (oneBtn && oneCart && hidden) {
+    oneBtn.addEventListener('click', function () {
+        oneCart.style.display = 'block';
+        oneBtn.classList.add('frequently__btn__hover');
+    });
+
+    hidden.addEventListener('click', function () {
+        oneCart.style.display = 'none';
+        oneBtn.classList.remove('frequently__btn__hover');
+    });
+}
+
+let oneeBtn = document.querySelector('#oneeBtn');
+let oneeCart = document.querySelector('#oneeCart');
+let hiddeen = document.querySelector('#hiddeen');
+
+if (oneeBtn && oneeCart && hiddeen) {
+    oneeBtn.addEventListener('click', function () {
+        oneeCart.style.display = 'block';
+        oneeBtn.classList.add('frequently__btn__hover');
+    });
+
+    hiddeen.addEventListener('click', function () {
+        oneeCart.style.display = 'none';
+        oneeBtn.classList.remove('frequently__btn__hover');
+    });
+}
+
+let uchBtn = document.querySelector('#uchBtn');
+let uchCart = document.querySelector('#uchCart');
+let uchhidden = document.querySelector('#uchhidden');
+
+if (uchBtn && uchCart && uchhidden) {
+    uchBtn.addEventListener('click', function () {
+        uchCart.style.display = 'block';
+        uchBtn.classList.add('frequently__btn__hover');
+    });
+
+    uchhidden.addEventListener('click', function () {
+        uchCart.style.display = 'none';
+        uchBtn.classList.remove('frequently__btn__hover');
+    });
+}
+
+let fourBtn = document.querySelector('#fourBtn');
+let fourCart = document.querySelector('#fourCart');
+let hidden3 = document.querySelector('#hidden3');
+
+if (fourBtn && fourCart && hidden3) {
+    fourBtn.addEventListener('click', function () {
+        fourCart.style.display = 'block';
+        fourBtn.classList.add('frequently__btn__hover');
+    });
+
+    hidden3.addEventListener('click', function () {
+        fourCart.style.display = 'none';
+        fourBtn.classList.remove('frequently__btn__hover');
+    });
+}
