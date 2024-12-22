47de31b162fc56ac84c5253e6ad60a2053840ff5
Muhammadsol1h <mmuhammadsolih0@gmail.com>
2024-12-21 21:21:34 -0800

    add

--- js/app.js
+++ js/app.js
@@ -4,10 +4,21 @@ let headerRes = document.querySelector('.header__res');
 let heroCart = document.querySelector('.hero__cart')
 
 headerMenu.addEventListener('click', function () {
-    headerRes.classList.add('active'); 
+    headerRes.classList.add('active');
     heroCart.classList.add('activeBanner')
 });
 
 headerResClose.addEventListener('click', function () {
     headerRes.classList.remove('active');
 });
+
+
+let featuresBtn = document.querySelectorAll('.features__wrap__btn');
+
+featuresBtn.forEach((item) => {
+    item.addEventListener('click', function () {
+        featuresBtn.forEach((btn) => btn.classList.remove('features__wrap__btn__active'));
+        item.classList.add('features__wrap__btn__active');
+    });
+});
+
