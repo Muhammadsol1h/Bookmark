a178c0e12c8e9b5142c7dc3306f0847fbf012968
Muhammadsol1h <mmuhammadsolih0@gmail.com>
2024-12-23 00:54:05 -0800

    add

--- js/app.js
+++ js/app.js
@@ -22,6 +22,33 @@ featuresBtn.forEach((item) => {
     });
 });
 
+let featureswrapbtn = document.querySelector('.features__wrap__btn')
+let featuresBtnOne = document.querySelector('#featuresBtnOne')
+let featuresBtnTwo = document.querySelector('#featuresBtnTwo')
+
+let simpleCart = document.querySelector('.simpleCart')
+let speedyCart = document.querySelector('.speedyCart')
+let easyCart = document.querySelector('.easyCart')
+
+featureswrapbtn.addEventListener('click' , function(){
+    simpleCart.style.display = 'block'
+    speedyCart.style.display = 'none'
+    easyCart.style.display = 'none'
+})
+
+featuresBtnOne.addEventListener('click' , function(){
+    simpleCart.style.display = 'none'
+    speedyCart.style.display = 'block'
+    easyCart.style.display = 'none'
+})
+
+featuresBtnTwo.addEventListener('click' , function(){
+    simpleCart.style.display = 'none'
+    speedyCart.style.display = 'none'
+    easyCart.style.display = 'block'
+})
+
+
 let oneBtn = document.querySelector('#oneBtn');
 let oneCart = document.querySelector('#oneCart');
 let hidden = document.querySelector('#hidden');
